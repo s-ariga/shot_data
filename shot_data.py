62c7543742afc1ff74be02a1dccf0f9c53097498
s-ariga <seiichi.ariga@gmail.com>
2020-01-02 11:59:18 +0900

    Y軸も表示する

--- shot_data.py
+++ shot_data.py
@@ -7,12 +7,15 @@ import pylab as plb
 # shot-data.csv has column 'CoordinateX' and 'CoordinateY'
 data = pd.read_csv("shot-data.csv")
 
-param = norm.fit(data['CoordinateX'])
-print(param)
+paramX = norm.fit(data['CoordinateX'])
+paramY = norm.fit(data['CoordinateY'])
+print(paramX)
+print(paramY)
 
 # np.linspace(下限, 上限, 要素数)
 x = np.linspace(-20, 20, 200)
-pdf_fitted = norm.pdf(x, loc=param[0], scale=param[1])
+pdf_fittedX = norm.pdf(x, loc=paramX[0], scale=paramX[1])
+pdf_fittedY = norm.pdf(x, loc=paramY[0], scale=paramY[1])
 pdf = norm.pdf(x)
 
 fig = plt.figure()
@@ -20,6 +23,12 @@ fig = plt.figure()
 plt.title('Histogram of AR 5970 shots')
 plt.xlabel('mm')
 plt.ylabel('freq')
-plt.plot(x, pdf_fitted)
-plt.hist(data['CoordinateX'], bins=200, range=(-20, 20), density=True)
+plt.plot(x, pdf_fittedX, label="PDF CoordX")
+plt.plot(x, pdf_fittedY, label="PDF CoordY")
+plt.hist(data['CoordinateX'], bins=200, range=(-20, 20), density=True, label="CoordX")
+plt.hist(data['CoordinateY'], bins=200, range=(-20, 20), density=True, label="CoordY")
+
+plt.legend()
+
+# Keep matplotlib on screen
 plt.show()
